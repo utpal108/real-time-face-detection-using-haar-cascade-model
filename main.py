99a6e02027a7e105ea82fe71e1bdc7760a039e6a
Utpal Paul <utpalseu@gmail.com>
2023-10-25 20:28:33 +0600

    Video Capture Option Updated

--- main.py
+++ main.py
@@ -1,25 +1,33 @@
 import cv2
 
-harcascade = "model/haarcascade_frontalface_default.xml"
+face_cascade = cv2.CascadeClassifier('model/haarcascade_frontalface_default.xml')
 
 cap = cv2.VideoCapture(0)
 cap.set(3, 640) # Width
 cap.set(4, 480) # Height
 
 while True:
-    status, frame = cap.read()
+    ret, frame = cap.read()
 
-    facecascade = cv2.CascadeClassifier(harcascade)
-    frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    if not ret:
+        break
 
-    face = facecascade.detectMultiScale(frame_gray, 1.1, 4)
+    # Image Processing Technique In Here
+    gray_image = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray_image, scaleFactor=1.1, minNeighbors=5, minSize=(30,30))
 
-    for (x,y,w,h) in face:
-        cv2.rectangle(frame, (x,y), (x+w,y+h), (0,255,0), 2)
+    for (x,y,w,h) in faces:
+        cv2.rectangle(frame,(x,y), (x+w,y+h), (0,255,0), 5)
+    
 
     cv2.imshow('Face Detection', frame)
 
+
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
+
+cap.release()
+cv2.destroyAllWindows()
+    
 
 
